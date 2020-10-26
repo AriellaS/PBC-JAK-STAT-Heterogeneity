a93f189ca171fb82a49e9194111a4d730c12fe1a
AriellaS <ariella.simoni@gmail.com>
2020-10-25 22:15:58 -0700

    Put calculated quantities in seperate file

--- runModel_withFits_struct8.m
+++ runModel_withFits_struct8.m
@@ -8,7 +8,7 @@ type = 'weighted';
 Data_time = importdata('tspan.mat');
 
 %--- Model prediction timepoints
-predTime = [0:60:24*3600];
+predTime = [0:60:6*3600];
 
 Data_time_minutes = Data_time./60;
 predTime_minutes =predTime./60;
@@ -275,25 +275,25 @@ params(61,1) = k35;
 
 options = odeset('RelTol',1e-9,'AbsTol',1e-12,'NonNegative',[1:length(initvalues)]);
 
-% simulate the model
+%% simulate the model
 
-parfor n = 1 : num_samples
-	disp(n);
+% parfor n = 1 : num_samples
+% 	disp(n);
+%
+% 	selected_initvalues = initvalues;
+% 	selected_initvalues(2,1) = free_initValues(n,1);
+% 	selected_initvalues(5,1) = free_initValues(n,2);
+% 	selected_initvalues(6,1) = free_initValues(n,3);
+% 	selected_initvalues(7,1) = free_initValues(n,4);
+%
+% 	[~, predConc] = ode15s(@core_file_struct8,predTime,selected_initvalues,options,params);
+% 	results(n,:,:) = predConc;
+% end
 
-	selected_initvalues = initvalues;
-	selected_initvalues(2,1) = free_initValues(n,1);
-	selected_initvalues(5,1) = free_initValues(n,2);
-	selected_initvalues(6,1) = free_initValues(n,3);
-	selected_initvalues(7,1) = free_initValues(n,4);
+%% simulate with ramping PRL
 
-	[~, predConc] = ode15s(@core_file_struct8,predTime,selected_initvalues,options,params);
-	results(n,:,:) = predConc;
-end
-
-% simulate with ramping PRL
-
-% prl_vals = [50, 100, 200, 300];
-prl_vals = [200, 0, 0, 0];
+prl_vals = [50, 100, 200, 300];
+% prl_vals = [200, 0, 0, 0];
 
 for sim = 1 : length(prl_vals)
 	disp(sim);
@@ -306,63 +306,21 @@ for sim = 1 : length(prl_vals)
 			selected_initvalues(6,1) = free_initValues(n,3);
 			selected_initvalues(7,1) = free_initValues(n,4);
 		else
+num_samples = 5;
 			selected_initvalues = initvalues(n,:)';
 		end
 		selected_initvalues(1,1) = selected_initvalues(1,1) + prl_vals(sim) / 22;
 		[~, predConc] = ode15s(@core_file_struct8,predTime,selected_initvalues,options,params);
-		results(sim,n,:,:) = predConc;
+		segmented_results(sim,n,:,:) = predConc;
 	end
-	initvalues = squeeze(results(sim,:,end,:));
+	initvalues = squeeze(segmented_results(sim,:,end,:));
 end
 
-
-% Calculated quantities
-
-cytosolic_pStatA = results(:,:,13) + 2.*results(:,:,15) + results(:,:,16) + results(:,:,19) + 2*results(:,:,21) + results(:,:,23) + results(:,:,24) + results(:,:,27);
-cytosolic_pStatB = results(:,:,14) + 2.*results(:,:,17) + results(:,:,16) + results(:,:,20) + 2*results(:,:,22) + results(:,:,23) + results(:,:,25) + results(:,:,26);
-
-nuclear_pStatA = 2*results(:,:,28) + results(:,:,29) + results(:,:,31) + results(:,:,33) + 2*results(:,:,37) + results(:,:,38) + results(:,:,40) + results(:,:,43);
-nuclear_pStatB = 2*results(:,:,30) + results(:,:,29) + results(:,:,32) + results(:,:,34) + 2*results(:,:,39) + results(:,:,38) + results(:,:,41) + results(:,:,42);
-
-npA_ratio_cpA = nuclear_pStatA./cytosolic_pStatA;
-npB_ratio_cpB = nuclear_pStatB./cytosolic_pStatB;
-
-total_pStatA = results(:,:,13) +2.*results(:,:,15) + results(:,:,16) + results(:,:,19) + 2*results(:,:,21) + results(:,:,23) + results(:,:,24) + results(:,:,27) + 2*results(:,:,28) + results(:,:,29) + results(:,:,31) + results(:,:,33) + 2*results(:,:,37) + results(:,:,38) + results(:,:,40) + results(:,:,43);
-total_pStatB = results(:,:,14) +2.*results(:,:,17) + results(:,:,16) + results(:,:,20) + 2*results(:,:,22) + results(:,:,23) + results(:,:,25) + results(:,:,26) + 2*results(:,:,30) + results(:,:,29) + results(:,:,32) + results(:,:,34) + 2*results(:,:,39) + results(:,:,38) + results(:,:,41) + results(:,:,42);
-
-% pStatA_norm(:,:,i) = total_pStatA(:,:,i)./total_pStatA(31,i);% normalized to 30 minute
-% pStatB_norm(:,:,i) = total_pStatB(:,:,i)./total_pStatB(31,i);% normalized to 30 minute
-
-cytosolic_statA = results(:,:,3) + results(:,:,11) + results(:,:,13) + 2*results(:,:,15) + results(:,:,16) + results(:,:,19) + 2*results(:,:,21) + results(:,:,23) + 2*results(:,:,24) + results(:,:,26) + results(:,:,27) + results(:,:,48);
-cytosolic_statB = results(:,:,4) + results(:,:,12) + results(:,:,14) + 2*results(:,:,17) + results(:,:,16) + results(:,:,20) + 2*results(:,:,22) + results(:,:,23) + 2*results(:,:,25) + results(:,:,26) + results(:,:,27) + results(:,:,49);
-
-nuclear_statA = 2*results(:,:,28) + results(:,:,29) + results(:,:,31) + results(:,:,33) + results(:,:,35) + 2*results(:,:,37) + results(:,:,38) + 2*results(:,:,40) + results(:,:,42) + results(:,:,43);
-nuclear_statB = 2*results(:,:,30) + results(:,:,29) + results(:,:,32) + results(:,:,34) + results(:,:,36) + 2*results(:,:,39) + results(:,:,38) + 2*results(:,:,41) + results(:,:,42) + results(:,:,43);
-
-% total_statA = nuclear_statA*Vratio + cytosolic_statA;
-% total_statB = nuclear_statB*Vratio + cytosolic_statB;
-
-npAcpA_ratio_totA = (nuclear_pStatA*Vratio + cytosolic_pStatA)./totalSTAT;
-npBcpB_ratio_totB = (nuclear_pStatB*Vratio + cytosolic_pStatB)./totalSTAT;
-
-% nucleus_cyto_ratioA(:,i) = Stat_nucleusA(:,i)./Stat_cytoA(:,i);
-% nucleus_cyto_ratioB(:,i) = Stat_nucleusB(:,i)./Stat_cytoB(:,i);
-
-% transloc_predA(:,i) = nucleus_cyto_ratioA(:,i);
-% transloc_predB(:,i) = nucleus_cyto_ratioB(:,i);
-
-% rec_total(:,i) = predConc(:,2) + predConc(:,8) + 2*predConc(:,9) + 2*predConc(:,10) + 2*predConc(:,11) + 2*predConc(:,12) + 2*predConc(:,18) + 2*predConc(:,47) + 2*predConc(:,48) + 2*predConc(:,49) + 2*predConc(:,50);
-% rec_internalized = 1 - rec_total(:,i)./initvalues(2);
-% intern_pred(i) = rec_internalized(31);
-
-% Bcl fold change
-% Bcl(:,i) = predConc(:,56)./predConc(1,56);
-
- % pJAK2
-% total_pJAK2 = predConc(:,10) + predConc(:,11)+ predConc(:,12)+ predConc(:,18)+ predConc(:,47)+ predConc(:,48)+ predConc(:,49)+ predConc(:,50);
-% pJAK2_norm(:,i) = total_pJAK2./total_pJAK2(11); %Normalized to 10 minute
-
-
+results = zeros(num_samples,1441,56);
+results(:,1:361,:) = segmented_results(1,:,:,:);
+results(:,361:721,:) = segmented_results(2,:,:,:);
+results(:,721:1081,:) = segmented_results(3,:,:,:);
+results(:,1081:1441,:) = segmented_results(4,:,:,:);
 
 %% Plot results
 
