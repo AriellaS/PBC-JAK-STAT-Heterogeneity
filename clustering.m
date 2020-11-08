1a926e96f49b0b83ce367c4aefbae8705aa41fc6
AriellaS <ariella.simoni@gmail.com>
2020-11-08 12:56:35 -0800

    Add progress indicator for clustering

--- clustering.m
+++ clustering.m
@@ -3,27 +3,27 @@ cluster.JobStorageLocation='/home1/ariellas/matlabJobStorage';
 [a,b]=evalc('system(''nproc --all'')');
 cluster.NumWorkers=str2num(a);
 
-% global indx
+global progress
 
-load('data/results/outputs_24hrs.mat');
+load('data/results/outputs_rampedPRL_24hrs.mat');
 n_samples = 1000;
 pool = parpool(cluster,15);
 
 for output = 1 : 4
-	% indx = 1;
+	progress = 1;
 	disp(output)
 	data = squeeze(outputs(output,1:n_samples,:));
-	D = pdist(data, @(x,y) dtwdist(x,y));
+	D = pdist(data, @(x,y) dtwdist(x,y,output));
 	Z(output,:,:) = linkage(D);
 end
 
-save('data/results/linkages_24hrs','Z');
+save('data/results/linkages_rampedPRL_24hrs','Z');
 delete(pool);
 
-function d = dtwdist(x,y)
-	% global indx
-	% disp(indx)
-	% indx = indx + 1;
+function d = dtwdist(x,y,output)
+	global progress
+	progress = progress + 1;
+	disp(num2str(output) + " " + num2str(progress/n_samples * 100))
 	[m,n] = size(y);
 	d = zeros(m,1);
 	parfor j = 1 : m
