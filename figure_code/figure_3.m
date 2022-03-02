059378ee9d212f4cef63485f175650262e82de59
AriellaS <ariella.simoni@gmail.com>
2022-03-01 19:10:43 -0800

    Update figure numbering to match paper

--- figure_code/figure_3.m
+++ figure_code/figure_3.m
@@ -2,33 +2,19 @@ clear all; clc; close all;
 
 lowest_error_responses = importdata('../holly_results/lowest_error_responses.mat');
 
+predTime = [0:60:6*3600];
 response_labels = {"Nuclear/cytosolic ratio pSTAT5A","Nuclear/cytosolic ratio pSTAT5B","Relative concentration pSTAT5A","Relative concentration pSTAT5B"};
 
-nbins = 50;
-c = [0.5 0.5 0.5];
-
 figure(1);
-for i = 1 : 4
+for i = 1 : length(response_labels)
 	subplot(2,2,i);
-	response = log10(lowest_error_responses(:,end,:));
-	histogram(response(:,i),nbins,'facecolor',c);
-	xmin1 = min(response(:,i));
-	xmax1 = max(response(:,i));
-	if mod(i,2) == 0
-		xmin2 = min(response(:,i-1));
-		xmax2 = max(response(:,i-1));
-	else
-		xmin2 = min(response(:,i+1));
-		xmax2 = max(response(:,i+1));
-	end
-	xmin = min(xmin1,xmin2);
-	xmax = max(xmax1,xmax2);
-	xlim([xmin-0.1 xmax+0.1])
+	y = lowest_error_responses(:,:,i);
+	plot(predTime/3600,y./max(y,[],2),'color',[0 0 0 0.01]);
 	title(response_labels(i));
 	if i > 2
-		xlabel("Log_{10} (response)");
-	end
-	if mod(i,2) ~= 0
-		ylabel("Number of cells")
+		xlabel("Time (hours)");
 	end
 end
+% sgtitle("Normalized time courses using lowest error parameters");
+
+
--- figure_code/figure_4.m
+++ figure_code/figure_4.m
@@ -1,29 +1,34 @@
-clear all;
+clear all; clc; close all;
 
-response_labels = [{"Nuclear/cytosolic";"ratio pSTAT5A"},{"Nuclear/cytosolic";"ratio pSTAT5B"},{"Relative conc.";"pSTAT5A"},{"Relative conc.";"pSTAT5B"}];
-initval_labels = ["RJ","SHP2","PPX","PPN"];
-num_responses = length(response_labels);
-num_initvals = length(initval_labels);
+lowest_error_responses = importdata('../holly_results/lowest_error_responses.mat');
 
-c = [0 0 0];
+response_labels = {"Nuclear/cytosolic ratio pSTAT5A","Nuclear/cytosolic ratio pSTAT5B","Relative concentration pSTAT5A","Relative concentration pSTAT5B"};
 
-responses = importdata('../holly_results/lowest_error_responses.mat');
-responses = responses(:,361,:);
-free_initvals = importdata('../error_ranked_parameters/lowest_error_free_initvalues.mat');
+nbins = 50;
+c = [0.5 0.5 0.5];
 
-figure(1)
-for i = 1 : num_responses
-	for j = 1 : num_initvals
-		subplot(num_responses,num_initvals,num_initvals*(i-1)+j)
-		scatter(free_initvals(:,j),responses(:,i),10,c,'filled','markerfacealpha',0.2);
-		if (i==num_responses)
-			xlabel({initval_labels(j);"initial concentration";" ";" "},'fontweight','bold','fontsize',11,'Rotation',0)
-		end
-		if (j==1)
-			ylabel(response_labels(:,i),'fontweight','bold','fontsize',10)
-		end
+figure(1);
+for i = 1 : 4
+	subplot(2,2,i);
+	response = log10(lowest_error_responses(:,end,:));
+	histogram(response(:,i),nbins,'facecolor',c);
+	xmin1 = min(response(:,i));
+	xmax1 = max(response(:,i));
+	if mod(i,2) == 0
+		xmin2 = min(response(:,i-1));
+		xmax2 = max(response(:,i-1));
+	else
+		xmin2 = min(response(:,i+1));
+		xmax2 = max(response(:,i+1));
+	end
+	xmin = min(xmin1,xmin2);
+	xmax = max(xmax1,xmax2);
+	xlim([xmin-0.1 xmax+0.1])
+	title(response_labels(i));
+	if i > 2
+		xlabel("Log_{10} (response)");
+	end
+	if mod(i,2) ~= 0
+		ylabel("Number of cells")
 	end
 end
-set(gcf,'Position',[100 60 800 600])
-
-
